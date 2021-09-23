8e5c9911eb97d72ebe2c27389680377a44cefc34
Anais Schlienger <anais.schlienger@lilo.org>
2021-09-23 10:09:26 +0200

    :tada: credits page

---
+++ src/views/Credits.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="tokyo_tm_news">
+    <div class="article_title">
+      <div class="title_flex">
+        <div class="left">
+          <span>Credits</span>
+          <!-- <h3>Latest News</h3> -->
+        </div>
+      </div>
+    </div>
+    <!-- END TITLE -->
+    <div class="tokyo_tm_modalbox_news">
+      
+      <div class="details"><div class="extra"></div></div>
+
+      <div class="main_content ">
+        <div class="descriptions"> 
+          <h3 class="article_subtitle">
+            Mentions légales       
+          </h3>  
+          <p>
+            Le site www.simonemarck.com est édité par Simone Marck, également responsable éditoriale et 
+            titulaire de tous les droits de propriété intellectuelle sur l'ensemble des contenus. 
+            Toute reproduction partielle ou totale du contenu de ce site est soumise à une autorisation 
+            préalable de la part de son éditrice.
+          </p>
+          <h3 class="article_subtitle">
+            Editeur       
+          </h3> 
+          <p>
+            Simone Marck
+            2, rue de la Trinité
+            67190 Mutzig, France
+            Tél. : 06 13 37 64 66
+
+            Numéro de Siret [.....]
+            Code NAF [.....]
+          </p>
+          <h3 class="article_subtitle">
+            Responsable éditorial       
+          </h3> 
+          <p>
+            Simone Marck
+          </p>
+          <h3 class="article_subtitle">
+            Crédits       
+          </h3> 
+          <p>
+            <b>Textes</b>
+            Éliane Autran
+
+            <b>Photographies</b>
+            Simone Marck
+            Anaïs Schlienger
+            ou mentionné en légende si autre
+
+            <b>Conception-réalisation</b>
+            Anaïs Schlienger
+            270 rue Saint-Jacques
+            75005 Paris, France
+            Tél. : 06 26 63 96 58
+            Courriel : anais.schlienger@lilo.org
+         </p>
+          
+          <h3 class="article_subtitle">
+            Propriété intellectuelle         
+          </h3>  
+          <p>
+           L'éditeur du site est titulaire exclusif de tous les droits de propriété intellectuelle 
+           sur les contenus du site www.eliane-autran.fr. Le code de la propriété intellectuelle interdit 
+           les copies ou reproductions destinées à une utilisation collective. 
+           Toute représentation ou reproduction intégrale ou partielle faite par quelque procédé 
+           que ce soit, sans le consentement de l'éditeur du site est illicite et constitue une 
+           contrefaçon sanctionnée par les articles 425 et suivants du Code pénal.
+          </p>
+          <h3 class="article_subtitle">
+            Informatique et libertés        
+          </h3> 
+          <p>
+            Conformément aux préconisations de la Cnil et à la loi Informatique et Libertés n° 78-17 
+            du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous disposez 
+            d'un droit de consultation, de rectification et de suppression des données personnelles 
+            vous concernant communiquées à notre société. Pour l'exercer, adressez-vous au webmestre 
+            du site via le formulaire de contact.
+          </p>
+        </div>
+        <!-- END DESCRIPTION --> 
+        <div class="footer">
+          <div class="details"><div class="extra"></div></div>
+          <p>Par Simone Marck</p>
+        </div>                  
+      </div>
+      <!-- END CONTENT -->
+    </div>
+    <!-- END ARTICLE TEXT -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
