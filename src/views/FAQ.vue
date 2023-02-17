08a5b4d4ad1fd3e0e970109941b131fc8fcb915f
Ana Schlienger <anaiss@theodo.com>
2023-02-17 16:39:29 -0500

    augmenter tarif reflexo

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -4,29 +4,42 @@
       <span>FOIRE AUX QUESTIONS</span>
     </div>
 
-    <Question :question="'Quels sont les tarifs pratiqués ?'" 
-              :answer1="'1 séance de réflexologie plantaire : 35€ '"
-              :answer2="'1 massage Hakim : 40€'"
-              :answer3="'1 séance de relaxation : 20€'" />
-    <Question :question="'Les séances sont-elles remboursées ?'" 
-              :answer1="'Le remboursement des séances de réflexologie est possible selon votre mutuelle (voir votre contrat).'" />
-    <Question :question="'Quelle est la durée d\'une séance ?'" 
-              :answer1="'Une séance de réflexologie dure 45 minutes,' + 
-                      'auxquelles il faut ajouter le temps de faire connaissance et le temps de s\'installer.'" 
-              :answer2="'Massage Hakim : le massage dure environ 25 minutes.'" 
-              :answer3="'Relaxation : la séance varie entre 20 et 30 minutes.'"/>
-    <Question :question="'Peut-on offrir des séances ?'" 
-              :answer1="'Vous avez la possibilité de prendre des bons cadeaux, que vous pouvez offrir aux anniversaires, ' +
-                      'à la fête des mères... Pour faire plaisir et se faire plaisir'" />
-    <Question :question="'Quels sont les produits utilisés ?'" 
-              :answer1="'Ce sont des huiles bio. Je prépare ma propre huile pour le massage Hakim'"
-              :answer2="'En réflexologie, je fais usage d\'huiles différentes selon les besoins.'" />
-
+    <Question
+      :question="'Quels sont les tarifs pratiqués ?'"
+      :answer1="'1 séance de réflexologie plantaire : 40€ '"
+      :answer2="'1 massage Hakim : 40€'"
+      :answer3="'1 séance de relaxation : 20€'"
+    />
+    <Question
+      :question="'Les séances sont-elles remboursées ?'"
+      :answer1="'Le remboursement des séances de réflexologie est possible selon votre mutuelle (voir votre contrat).'"
+    />
+    <Question
+      :question="'Quelle est la durée d\'une séance ?'"
+      :answer1="
+        'Une séance de réflexologie dure 45 minutes,' +
+        'auxquelles il faut ajouter le temps de faire connaissance et le temps de s\'installer.'
+      "
+      :answer2="'Massage Hakim : le massage dure environ 25 minutes.'"
+      :answer3="'Relaxation : la séance varie entre 20 et 30 minutes.'"
+    />
+    <Question
+      :question="'Peut-on offrir des séances ?'"
+      :answer1="
+        'Vous avez la possibilité de prendre des bons cadeaux, que vous pouvez offrir aux anniversaires, ' +
+        'à la fête des mères... Pour faire plaisir et se faire plaisir'
+      "
+    />
+    <Question
+      :question="'Quels sont les produits utilisés ?'"
+      :answer1="'Ce sont des huiles bio. Je prépare ma propre huile pour le massage Hakim'"
+      :answer2="'En réflexologie, je fais usage d\'huiles différentes selon les besoins.'"
+    />
   </div>
 </template>
 
 <script>
-import Question from "../components/Question.vue"
+import Question from "../components/Question.vue";
 export default {
   components: {
     Question,
